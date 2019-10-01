213c007121a087433dc469978f196860e2c87fc8
Shane <shane@localhost.localdomain>
2019-09-30 22:01:16 -0400

    Set up express/node backend

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/api/hello', function (req, res) {
+    res.send({ express: 'Hello From Express' });
+});
+app.post('/api/world', function (req, res) {
+    console.log(req.body);
+    res.send("I received your POST request. This is what you sent me: " + req.body.post);
+});
+app.listen(port, function () { return console.log("Listening on port " + port); });
