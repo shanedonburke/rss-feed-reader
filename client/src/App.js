dc590c429851cca4b0d746648be8b4c60e1d294a
Shane <shane@localhost.localdomain>
2019-09-30 22:20:14 -0400

    Integrated Express with React

---
+++ client/src/App.js
@@ -0,0 +1,137 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+exports.__esModule = true;
+var react_1 = __importStar(require("react"));
+var logo = require("./logo.svg");
+require("./App.css");
+var App = /** @class */ (function (_super) {
+    __extends(App, _super);
+    function App() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.state = {
+            response: '',
+            post: '',
+            responseToPost: ''
+        };
+        _this.callApi = function () { return __awaiter(_this, void 0, void 0, function () {
+            var response, body;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, fetch('/api/hello')];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.json()];
+                    case 2:
+                        body = _a.sent();
+                        if (response.status !== 200)
+                            throw Error(body.message);
+                        return [2 /*return*/, body];
+                }
+            });
+        }); };
+        _this.handleSubmit = function (e) { return __awaiter(_this, void 0, void 0, function () {
+            var response, body;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        e.preventDefault();
+                        return [4 /*yield*/, fetch('/api/world', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                },
+                                body: JSON.stringify({ post: this.state.post })
+                            })];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.text()];
+                    case 2:
+                        body = _a.sent();
+                        this.setState({ responseToPost: body });
+                        return [2 /*return*/];
+                }
+            });
+        }); };
+        return _this;
+    }
+    App.prototype.componentDidMount = function () {
+        var _this = this;
+        this.callApi()
+            .then(function (res) { return _this.setState({ response: res.express }); })["catch"](function (err) { return console.log(err); });
+    };
+    App.prototype.render = function () {
+        var _this = this;
+        return (react_1["default"].createElement("div", { className: "App" },
+            react_1["default"].createElement("header", { className: "App-header" },
+                react_1["default"].createElement("img", { src: logo, className: "App-logo", alt: "logo" }),
+                react_1["default"].createElement("p", null,
+                    "Edit ",
+                    react_1["default"].createElement("code", null, "src/App.js"),
+                    " and save to reload."),
+                react_1["default"].createElement("a", { className: "App-link", href: "https://reactjs.org", target: "_blank", rel: "noopener noreferrer" }, "Learn React")),
+            react_1["default"].createElement("p", null, this.state.response),
+            react_1["default"].createElement("form", { onSubmit: this.handleSubmit },
+                react_1["default"].createElement("p", null,
+                    react_1["default"].createElement("strong", null, "Post to Server:")),
+                react_1["default"].createElement("input", { type: "text", value: this.state.post, onChange: function (e) { return _this.setState({ post: e.target.value }); } }),
+                react_1["default"].createElement("button", { type: "submit" }, "Submit")),
+            react_1["default"].createElement("p", null, this.state.responseToPost)));
+    };
+    return App;
+}(react_1.Component));
+exports["default"] = App;
