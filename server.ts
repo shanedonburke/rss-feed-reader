c7044af3590be0cfe580fd7c5a7df9527c3b19e8
ydp4388 <ydp4388@rit.edu>
2019-10-01 14:04:53 -0400

    finally displays frontend with localhost

--- server.ts
+++ server.ts
@@ -1,11 +1,17 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
+var express = require('express');
+var $path = require('path') ;
+var bodyParser = require('body-parser');
+ 
+var app = express();
+var port = process.env.PORT || 5000;
+const publicDir = $path.resolve('./client/public');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('client/public')) ;
+
+app.get('/', function (req, res) {
+  res.sendFile($path.resolve(publicDir, 'index.html'));
+});
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
