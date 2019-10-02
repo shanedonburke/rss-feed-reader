b1620d8e05377db9684e15888f07897d95c996ef
Samuel Velasquez <sjv5866@rit.edu>
2019-10-02 12:12:01 -0400

    fixing newfeedinput

--- client/src/NewFeedInput.tsx
+++ client/src/NewFeedInput.tsx
@@ -24,16 +24,19 @@ class NewFeedInput extends Component<MyProps, MyState> {
         feed_url: this.state.value
     }
     
-    fetch('/api/add-new-feed', {
-      method: 'POST',
-      headers: {"Content-Type": "application/json"},
-      body: JSON.stringify(data),
-    });
+
+    //link contains data we want to parse! instead of sending a post request
+    //we first have to parse what was fetched from the link into workable data, THEN send a post request!!! 
+
+    //try posting this link to the textbox: https://jsonplaceholder.typicode.com/posts?userId=10
+    fetch(this.state.value)
+    .then(res => res.json())
+    .then(res => console.log(res))
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form method="post" action="./index.html" onSubmit={this.handleSubmit}>
         <label>
           Enter new feed URL: 
           <input type="text" name="url" placeholder="Enter URL here" value={this.state.value} onChange={this.handleChange} />
