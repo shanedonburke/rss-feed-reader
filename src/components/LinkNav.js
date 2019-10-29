620dd2d8e747322b1acae4d7beadd031e4f8d4b3
Samuel Velasquez <samuelvelasquez@protonmail.com>
2019-10-29 14:56:02 -0400

    fixed conflicts

--- src/components/LinkNav.js
+++ src/components/LinkNav.js
@@ -1,35 +1,22 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link } from 'react-router-dom';
-=======
-import React from 'react'
 import Nav from 'react-bootstrap/Nav'
-
->>>>>>> 7e95321f11e80e65f4beb5a9c9c7475c992caf90
 
 export default class LinkNav extends React.Component{
   render() {
     return(
-<<<<<<< HEAD
-      <div className="topnav">
-        <Link to="/">Your articles</Link>
-        <Link to="/add-article">Add article</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/account">Account</Link>
-      </div>
-=======
+
       <Nav variant="pills" defaultActiveKey="/home" fill>
       <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
+      <Link to="/"><Nav.Link href="/home">Active</Nav.Link></Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link eventKey="link-1">Favorite</Nav.Link>
+      <Link to="/favorites"><Nav.Link eventKey="link-1">Favorite</Nav.Link></Link>
       </Nav.Item>
       <Nav.Item>
-      < Nav.Link eventKey="link-2">Profile</Nav.Link>
+      <Link to="/profile">< Nav.Link eventKey="link-2">Profile</Nav.Link></Link>
       </Nav.Item>
     </Nav>
->>>>>>> 7e95321f11e80e65f4beb5a9c9c7475c992caf90
     )
   }
 }
