3e414c213de640b3bdb0466af53d945e8fa2f59e
Shane Burke <shanedonburke@gmail.com>
2019-11-05 14:36:09 -0500

    Listing feeds, commenting some things out for a merge to master

--- src/components/FeedList/FeedList.js
+++ src/components/FeedList/FeedList.js
@@ -1,47 +1,47 @@
-/*jshint esversion: 6 */
-"use-strict";
+// /*jshint esversion: 6 */
+// "use-strict";
 
-import React from "react";
-import FeedItem from "./FeedItem";
-import "./feedList.css";
+// import React from "react";
+// import FeedItem from "./FeedItem";
+// import "./feedList.css";
 
-function FeedList(props) {
-  return <div class="flex-container feed-list">{this.feedItems()}</div>;
-}
+// function FeedList(props) {
+//   return <div class="flex-container feed-list">{this.feedItems()}</div>;
+// }
 
-FeedList.componentDidMount = function() {
-  fetch('/some/async/data')
-    .then(resp => resp.json())
-    .then(data => this.setState({data}));
-}
+// FeedList.componentDidMount = function() {
+//   fetch('/some/async/data')
+//     .then(resp => resp.json())
+//     .then(data => this.setState({data}));
+// }
 
-FeedList.prototype.feedURLs = async function() {
-  const urls = await fetch("/get-feed-list")
-    .then(data => {
-      return data.json();
-    })
-    .then(res => {
-      return res.feedList;
-    })
-    .catch(err => {
-      return err;
-    });
+// FeedList.prototype.feedURLs = async function() {
+//   const urls = await fetch("/get-feed-list")
+//     .then(data => {
+//       return data.json();
+//     })
+//     .then(res => {
+//       return res.feedList;
+//     })
+//     .catch(err => {
+//       return err;
+//     });
 
-  return await urls;
-};
+//   return await urls;
+// };
 
-FeedList.prototype.feedItems = function() {
-  const response = await this.feedURLs()
-    .then(feedURLs => {
-      feedURLs.map((value, index) => {
-        return <FeedItem url={value} />;
-      });
-    })
-    .catch(err => {
-      return <div>There was an error obtaining the list of feed URLs.</div>;
-    });
+// FeedList.prototype.feedItems = function() {
+//   const response = this.feedURLs()
+//     .then(feedURLs => {
+//       feedURLs.map((value, index) => {
+//         return <FeedItem url={value} />;
+//       });
+//     })
+//     .catch(err => {
+//       return <div>There was an error obtaining the list of feed URLs.</div>;
+//     });
 
-    return await response;
-};
+//     return await response;
+// };
 
-export default FeedList;
+// export default FeedList;
