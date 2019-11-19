e030e57f66e10b0c8be17bbd5931333210089d25
ydp4388 <ydp4388@rit.edu>
2019-11-19 14:45:19 -0500

    Added routes to App.js

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
 
         <LinkNav />
         <Link to="/my-feeds">Hi</Link>
+        <Route exact path="/addArticles"><NewFeedInput/></Route>
         <Switch>
-          <Route exact path="/">
-            <NewFeedInput />
+          <Route exact path="/home">
             <div className="row">
               <div className="column">
                 <h2>Article 1</h2>
