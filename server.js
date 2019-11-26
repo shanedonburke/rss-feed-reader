bcafc8759cb108075be682274f88b7e0aca39b5f
Samuel Velasquez <samuelvelasquez@protonmail.com>
2019-11-26 14:38:20 -0500

    revert changes on server.js

--- server.js
+++ server.js
@@ -16,41 +16,28 @@ app.get('/', function (req, res) {
 });
 
 app.post('/add-feed', function (req, res) {
-    const jsonFeed = $path.join(publicDir, '/data/currentFeed.json');
-    fs.truncate(jsonFeed, 0, () => {
-        fs.writeFileSync(jsonFeed, JSON.stringify(req.body.feed, null, 2), {encoding: 'utf8',flag:'w'});
-    });
-    
-    // const feedURL = req.body.feedURL;
-    // const feedBody = req.body.feed;
-    // feedList.addFeed(feedBody).then(
-    //     result => {
-    //         res.status(200).send(result);
-    //     },
-    //     error => {
-    //         res.status(500).send(error);
-    //     }
-    // );
+    const feedURL = req.body.feedURL;
+    feedList.addFeed(feedURL).then(
+        result => {
+            res.status(200).send(result);
+        },
+        error => {
+            res.status(500).send(error);
+        }
+    );
 });
 
-// app.get('/get-feed-list', function(req, res) {
-//     let feedListPromise = feedList.getFeedList();
+app.get('/get-feed-list', function(req, res) {
+    let feedListPromise = feedList.getFeedList();
 
-//     feedListPromise.then(
-//         feedList => {
-//             res.status(200).send(JSON.stringify(feedList));
-//         },
-//         error => {
-//             res.status(500).send(error);
-//         }
-//     );
-// });
-
-app.get('/get-feed-list', (req, res) => {
-    const jsonFeed = $path.join(publicDir, '/data/currentFeed.json');
-    const data = fs.readFileSync(jsonFeed);
-    const json = JSON.parse(data);
-    res.type('json').send(json);
+    feedListPromise.then(
+        feedList => {
+            res.status(200).send(JSON.stringify(feedList));
+        },
+        error => {
+            res.status(500).send(error);
+        }
+    );
 });
 
 app.listen(port, function () { return console.log("Listening on port " + port); });
